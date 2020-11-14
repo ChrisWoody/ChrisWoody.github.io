2c28bb69654992ba07df28aca3f73d71629670c6
Chris Wood <16053164+ChrisWoody@users.noreply.github.com>
2020-11-14 14:15:26 +0800

    Removed unused pages/code, added boilerplate for future pages

--- src/ProjectGrimaldus/Program.cs
+++ src/ProjectGrimaldus/Program.cs
@@ -1,12 +1,5 @@
-using System;
-using System.Net.Http;
-using System.Collections.Generic;
 using System.Threading.Tasks;
-using System.Text;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 
 namespace ProjectGrimaldus
 {
@@ -16,9 +9,6 @@ namespace ProjectGrimaldus
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
-
-            builder.Services.AddScoped(
-                sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
 
             await builder.Build().RunAsync();
         }
