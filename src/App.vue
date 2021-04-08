d2bd18f8d187e3fdd12010e671bd46dfdf5ccd6c
Chris Wood <16053164+ChrisWoody@users.noreply.github.com>
2021-04-08 19:57:47 +0800

    Implement home page with key info

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HomePage/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HomePage from './components/HomePage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HomePage
   }
 }
 </script>
@@ -25,4 +25,5 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
 </style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="hello">
+    <h1>Chris Wood</h1>
+    <p>
+        <a href="https://www.linkedin.com/in/chris-wood-520350163/" target="_blank" rel="noopener noreferrer">
+          <img class="socials" src="../assets/linkedin-icon.svg" alt="linkedin">
+        </a>
+        <a href="https://github.com/ChrisWoody/" target="_blank" rel="noopener noreferrer">
+          <img class="socials" src="../assets/github-icon.svg" alt="github">
+        </a>
+    </p>
+    <p>
+      Hello and welcome to my website! Here you'll find various projects I've worked on plus<br>
+      documentation on Azure or security topics. 
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener noreferrer">vue-cli documentation</a>.
+    </p>
+
+    <h3 class="websitewip">Website status</h3>
+    <p>I'm in the process of migrating this website to Vue so you won't see everything here just yet <br>
+    but you can see the most interesting projects below.</p>
+
+    <h3>Games</h3>
+    <p><a href="https://chriswoodcodes.net/SpearCat/" target="_blank" rel="noopener noreferrer">Spear Cat</a></p>
+    <p><a href="https://chriswoodcodes.net/project-sicarius-build/" target="_blank" rel="noopener noreferrer">Project Sicarius</a></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.websitewip {
+    color: orange;
+}
+.socials {
+  margin: 0px 10px 0px 10px;
+  width: 30px;
+}
+
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
