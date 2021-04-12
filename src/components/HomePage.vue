fc1ec66c7215591f02c922a34da0f09c8ba23c6d
Chris Wood <16053164+ChrisWoody@users.noreply.github.com>
2021-04-12 20:44:28 +0800

    Update other sections with their own cards on the home page

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -2,8 +2,7 @@
   <div class="hello">
 
 <h3 class="websitewip">Website status</h3>
-    <p>I'm in the process of migrating this website to Vue so you won't see everything here just yet <br>
-    but you can see the most interesting projects below.</p>
+    <p>I'm in the process of migrating this website to Vue so you won't see everything here just yet but you can see the most interesting projects below.</p>
 
     <h1>Chris Wood</h1>
     <p>
@@ -15,8 +14,7 @@
         </a>
     </p>
     <p>
-      Hello and welcome to my website! Here you'll find various projects I've worked on plus<br>
-      documentation on Azure or security topics.
+      Hello and welcome to my website! Here you'll find various projects I've worked on plus documentation on Azure or security topics.
     </p>
 
     <h2>Games</h2>
@@ -48,10 +46,48 @@
     </v-container>
 
     <h2>Programming</h2>
-    <p><a href="https://github.com/ChrisWoody/ChrisWoody.github.io" target="_blank" rel="noopener noreferrer">This website!</a></p>
+    <v-container style="max-width: 1000px">
+        <v-row no-gutters class="justify-center">
+            <v-col v-for="programming in programmings" :key="programming.title" cols="1" class="widecard">
+                <v-card class="pa-2 cardmargin" max-width="300">                    
+                    <v-img :src="programming.imagesrc" class="white--text align-end" gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.4)">
+                        <v-card-title v-text="programming.title"></v-card-title>
+                    </v-img>
+                    <v-card-text v-text="programming.description"></v-card-text>
+                    <v-card-actions>
+                        <v-btn color="secondary" elevation="2" :href="programming.githuburl" target="_blank" rel="noopener noreferrer">
+                            <v-icon left>
+                                mdi-github
+                            </v-icon>
+                            View Code
+                        </v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-col>
+        </v-row>
+    </v-container>
 
     <h2>Learning</h2>
-    <p><a href="https://www.slideshare.net/ChrisWood262/oauth-2" target="_blank" rel="noopener noreferrer">OAuth 2 slides from a presentation I did</a></p>
+    <v-container style="max-width: 1000px">
+        <v-row no-gutters class="justify-center">
+            <v-col v-for="learning in learnings" :key="learning.title" cols="1" class="widecard">
+                <v-card class="pa-2 cardmargin" max-width="300">                    
+                    <v-img :src="learning.imagesrc" class="white--text align-end" gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.4)">
+                        <v-card-title v-text="learning.title"></v-card-title>
+                    </v-img>
+                    <v-card-text v-text="learning.description"></v-card-text>
+                    <v-card-actions>
+                        <v-btn color="secondary" elevation="2" :href="learning.githuburl" target="_blank" rel="noopener noreferrer">
+                            <v-icon left>
+                                mdi-presentation
+                            </v-icon>
+                            View
+                        </v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-col>
+        </v-row>
+    </v-container>
 
   </div>
 </template>
@@ -65,6 +101,12 @@ export default {
       { title: "Spear Cat", imagesrc: require("../assets/spear-cat-icon.jpg"), description: "Play as a Cat with a Spear to fight off the endless alien hordes", playurl: "https://chriswoodcodes.net/SpearCat/", githuburl: "https://github.com/ChrisWoody/SpearCat/"},
       { title: "Project Sicarius", imagesrc: require("../assets/project-sicarius-icon.jpg"), description: "Play as an Angel who has to take out the invading Demons", playurl: "https://chriswoodcodes.net/project-sicarius-build/", githuburl: "https://github.com/ChrisWoody/project-sicarius/"},
       { title: "Simon Says", imagesrc: require("../assets/simon-says-icon.jpg"), description: "Play the classic Simon Says game", playurl: "https://chriswoodcodes.net/simon-says/", githuburl: "https://github.com/ChrisWoody/simon-says/"},
+    ],
+    programmings: [
+      { title: "Project Grimaldus", imagesrc: require('../assets/project-grimaldus-icon.jpg'), description: "This website! It's changed frameworks and looks over the years with the current plan to get it running fully in Vue", githuburl: "https://github.com/ChrisWoody/ChrisWoody.github.io"}
+    ],
+    learnings: [
+      { title: "OAuth 2 slides", imagesrc: require('../assets/oauth2-slides-icon.jpg'), description: "Slides for an internal presentation I did on OAuth 2, explaining the different grant types and why some are better than others, plus some detail around JWT tokens", url: "https://www.slideshare.net/ChrisWood262/oauth-2"}
     ]
   })
 }
